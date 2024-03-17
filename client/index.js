7efbd078b69efd52687103a5ed8ba70533936902
Kevin Murphy <murph212@yahoo.com>
2024-03-17 14:57:54 -0400

    client side containerized with docker

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-import './static/styles.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import "./static/styles.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App tab='home' />);
+root.render(<App tab="home" />);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,35 +1,37 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
 
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
   },
-
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
     }),
   ],
-
+  devServer: {
+    port: 4000,
+  },
   module: {
     rules: [
       {
         test: /.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
